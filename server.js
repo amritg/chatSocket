a2d4001f0f55675f244e40252c51858d590a651d
Amrit Gautam <gautamamrit24@gmail.com>
2017-02-03 14:38:28 +0200

    Finish Simple Chatting Application form socket.io site Example

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ var express = require('express');
 var morgan = require('morgan');
 
 var app = express();
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.use(morgan('dev'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000,function(){
+io.on('connection', function(socket){
+    console.log('A user connected');
+    socket.on('chatMessage', function(msg){
+        console.log('Message is: ' + msg);
+        io.emit('chatMessage', msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('A user disconnected');
+    });
+});
+
+http.listen(3000,function(){
     console.log("Application running at => localhost:3000")
 });
 
